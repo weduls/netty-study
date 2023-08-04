b0a5bb39c21ede351610199d358d8981b49a4832
wedul <wedul.chul@gmail.com>
2023-08-05 00:00:16 +0900

    netty server create

--- src/main/java/org/example/netty/NettyEchoServer.java
+++ src/main/java/org/example/netty/NettyEchoServer.java
@@ -5,34 +5,47 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.sctp.nio.NioSctpServerChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.concurrent.DefaultEventExecutorGroup;
 import io.netty.util.concurrent.EventExecutorGroup;
+import io.netty.util.concurrent.FutureListener;
 
 public class NettyEchoServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         EventLoopGroup parentGroup = new NioEventLoopGroup();
         EventLoopGroup childGroup = new NioEventLoopGroup(4);
         EventExecutorGroup eventExecutorGroup = new DefaultEventExecutorGroup(4);
 
         try {
-            ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(parentGroup, childGroup)
-                    .channel(NioSctpServerChannel.class)
+            ServerBootstrap serverBootstrap = new ServerBootstrap().group(parentGroup, childGroup)
+                    .channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<>() {
                         @Override
-                        protected void initChannel(Channel ch) throws Exception {
+                        protected void initChannel(Channel ch) {
                             ch.pipeline().addLast(
                                     eventExecutorGroup, new LoggingHandler()
                             );
                             ch.pipeline().addLast(
+                                    new StringEncoder(),
+                                    new StringDecoder(),
                                     new NettyServerHandler()
                             );
                         }
-                    })
-        } catch (Exception e) {
-
+                    });
+            serverBootstrap.bind(8080).sync()
+                    .addListener((FutureListener<Void>) future -> {
+                        if (future.isSuccess()) {
+                            System.out.print("success to bind 8080");
+                        } else {
+                            System.out.print("error");
+                        }
+                    }).channel().closeFuture().sync();
+        } finally {
+            parentGroup.shutdownGracefully();
+            childGroup.shutdownGracefully();
         }
     }
 }
--- src/main/java/org/example/netty/NettyServerHandler.java
+++ src/main/java/org/example/netty/NettyServerHandler.java
@@ -1,6 +1,16 @@
 package org.example.netty;
 
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof String) {
+            System.out.print("received msg");
+            ctx.writeAndFlush(msg)
+                    .addListener(ChannelFutureListener.CLOSE);
+        }
+    }
 }
